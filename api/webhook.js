5ffa26528eb72f763f254cc369e348127cef263c
Disa <disa@Disas-MacBook-Air.local>
2023-02-27 09:49:30 +0200

    remove node_modules hahah

--- api/webhook.js
+++ api/webhook.js
@@ -2,7 +2,6 @@ import { Telegraf } from 'telegraf';
 import { message } from 'telegraf/filters';
 import { ChatGPTAPI } from 'chatgpt';
 import { config } from 'dotenv';
-// import fetch from 'node-fetch';
 
 config();
 const SECRET_HASH = process.env.SECRET_HASH;
@@ -29,10 +28,9 @@ export default async function(req, res) {
     }
 
     if (query.secret_hash === SECRET_HASH) {
-      await bot.handleUpdate(body)
+      bot.handleUpdate(body)
     }
   } catch (e) {
-    console.error("Error sending message");
     console.log(e.toString());
   }
   res.status(200).send('OK');
